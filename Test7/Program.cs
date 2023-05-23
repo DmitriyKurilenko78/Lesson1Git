12833328d10498eb90fd40f89fb6200b7788d8a1
DmitriyKurilenko78 <Diamonddd@bk.ru>
2023-05-23 22:49:04 +0300

    задача с днями недели

--- Test7/Program.cs
+++ Test7/Program.cs
@@ -17,3 +17,18 @@ if (n >= 1 && n <= 5)
     Console.Write("Нет");
 else 
     Console.Write("Да");  
+
+// Эта задача с днями недели.
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// while ( n < 0 || n > 7)
+// {
+//     Console.Write("Вы ошиблись!\nВведите номер дня недели: ");
+//     n = int.Parse(Console.ReadLine()!);
+// }
+// if (n == 0)
+//     Console.WriteLine("Нет такого дня нелели");
+// List<string>week = new List<string>(){" ","Понедельник","Вторник","Среда","Четверг", "Пятница", "Суббота", "Воскресенье"};
+//     Console.WriteLine(week[n]);
