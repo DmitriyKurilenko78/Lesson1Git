a3567305463e7aa158ed8cbf1894410d23e35358
DmitriyKurilenko78 <Diamonddd@bk.ru>
2023-05-23 22:26:09 +0300

    решение задач 2, 4, 6, 8, 10, 13, 15

--- Test5/Program.cs
+++ Test5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход  трёхзначное 
+//              число и на выходе показывает вторую цифру этого числа.
+//            456 -> 5
+//            782 -> 8
+//            918 -> 1
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100 || n > 999)
+{
+      Console.Write("Вы ввели не верное число!\nВведите число: ");
+       n = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(n / 10 % 10);
--- Test7/Program.cs
+++ Test7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//            обозначающую день недели, и проверяет, является ли этот день выходным.
+//            6 -> да
+//            7 -> да
+//            1 -> нет
+
+
+Console.Clear();
+Console.Write("Введите число соответствующее дню недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+      Console.WriteLine("Не верное число\nВведите число: ");
+          n = int.Parse(Console.ReadLine()!);  
+}
+if (n >= 1 && n <= 5)
+    Console.Write("Нет");
+else 
+    Console.Write("Да");  
