c00a5beaa0880a8cfdbb2cb269aab0c24acad692
DmitriyKurilenko78 <Diamonddd@bk.ru>
2023-05-31 18:56:41 +0300

    Задача 27

--- Test12/Program.cs
+++ Test12/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int summa = 0;
+for (int i = 0; i < number.Length; i++)
+    summa = summa + Convert.ToInt32(number[i].ToString());
+Console.WriteLine(summa);
