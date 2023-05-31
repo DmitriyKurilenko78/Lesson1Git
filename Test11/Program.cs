9b4a382542e5e74ddbe0ee944002ca3a912499a0
DmitriyKurilenko78 <Diamonddd@bk.ru>
2023-05-31 18:47:23 +0300

    Задача 11

--- Test11/Program.cs
+++ Test11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не использовать Math.Pow
+// Задача 25: Напишите цикл, который принимает на вход два
+//  числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int s = 1;
+for (int i = 0; i < b; i++)
+    s = s * a;
+Console.WriteLine(s);
