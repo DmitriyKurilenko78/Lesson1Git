73e4226cc69236d09245d1aa9f39e2dc54f76d73
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-06-01 10:46:56 +0300

    Задача 29

--- Test13/Program.cs
+++ Test13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 29: Напишите программу, которая задаёт массив из 8 
+// элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear();
+Console.Write("Введите массив чисел: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] array = new int[n];
+
+  string input = Console.ReadLine();
+  string[] values = input.Split(',');
+
+        for (int i = 0; i < array.Length; i++) 
+         array[i] = int.Parse(values[i]);
+       
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
